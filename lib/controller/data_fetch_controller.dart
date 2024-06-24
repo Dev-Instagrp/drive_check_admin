0f7a26d55ef7634d11aa613c6e03058077b3f9d1
Dev-Instagrp <it.team.instaict@gmail.com>
2024-06-25 00:43:23 +0530

    Added feature to show OHS data on appropriate input, Updated approv and rejection of OHS followed by changing state

--- lib/controller/data_fetch_controller.dart
+++ lib/controller/data_fetch_controller.dart
@@ -7,39 +7,29 @@ class OHSCheckController extends GetxController {
   var selectedType = 'Activity Type'.obs;
   var selectedDate = DateTime.now().obs;
   var fetchedData = <String, dynamic>{}.obs;
+  var siteId = ''.obs;
+  var siteDate = ''.obs;
 
-  Future<void> fetchData() async {
+  Future<void> fetchData(String siteID) async {
     if (selectedEmployee.value == 'Select Employee' || selectedType.value == 'Activity Type') {
       showSnackbar('Error', 'Please select both employee and type');
       return;
     }
 
     String formattedDate = '${selectedDate.value.year}-${selectedDate.value.month.toString().padLeft(2, '0')}-${selectedDate.value.day.toString().padLeft(2, '0')}';
+    siteDate.value = '${selectedDate.value.day}-${selectedDate.value.month.toString()}-${selectedDate.value.year}';
 
     try {
-      QuerySnapshot userSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .where('Employee Name', isEqualTo: selectedEmployee.value)
-          .limit(1)
-          .get();
-
-      if (userSnapshot.docs.isEmpty) {
-        showSnackbar('Error', 'No user found');
-        return;
-      }
-
-      String userId = userSnapshot.docs.first.id;
-
       DocumentSnapshot dataSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(userId)
+          .collection('siteAllocation')
+          .doc(siteID+"(${siteDate})")
           .collection(selectedType.value)
           .doc(formattedDate)
           .get();
 
       if (dataSnapshot.exists) {
+        print(dataSnapshot);
         var data = dataSnapshot.data() as Map<String, dynamic>;
-        data['uid'] = userId;
         data['Employee Name'] = selectedEmployee.value;
         fetchedData.value = data;
       } else {
